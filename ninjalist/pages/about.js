a7824626f633ef0c26f0be415ddfadf07149c934
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-21 23:48:59 +0300

    added lecture 2: Pages and Routes

---
+++ ninjalist/pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolores, quisquam tempore quod fuga nihil ut neque numquam molestiae temporibus! Hic, nobis nihil. Ex reprehenderit a odit ipsam, consequuntur nihil?
+            Dolorum, aperiam quod dolores nihil autem sapiente fugit excepturi, maiores eveniet nulla rerum beatae veniam odit exercitationem natus a voluptatem assumenda aspernatur. In dolorum dicta error temporibus modi esse aspernatur!
+            Rem error culpa, pariatur ratione omnis eligendi repellat impedit enim asperiores quos cumque exercitationem facere nihil vel quia eveniet laudantium! Cupiditate, dicta similique illo non quasi ad fuga exercitationem delectus?
+            Delectus consequatur deserunt ut maiores quae, aperiam, nemo porro incidunt quam autem tempora animi molestias eligendi omnis blanditiis exercitationem! Quisquam error enim atque. Sed nihil, fugit est temporibus quae tempora.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo, hic eligendi obcaecati repudiandae iste totam dicta facilis unde veniam? Accusantium error quae architecto, aliquid commodi tenetur similique numquam ab!
+            Atque harum eligendi, minima vel molestiae tempora quaerat veritatis nam dicta vero pariatur corporis deleniti fugiat libero dolor sint ipsum qui doloribus possimus eveniet asperiores iusto aperiam nemo. Et, dicta.</p>
+        </div>
+    );
+}
+ 
+export default About;
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -3,20 +3,13 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ea deleniti dolore! Modi quo vero hic ut. Perspiciatis maiores error exercitationem? Odit, molestiae repellendus architecto debitis provident officiis sint ducimus?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio reiciendis, voluptate blanditiis asperiores incidunt optio vel voluptatum maiores. Placeat aut accusantium fugit in, modi temporibus dolor unde adipisci beatae quasi?</p>
+      
     </div>
   );
 }
