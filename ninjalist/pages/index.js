e6fd4707ddf3da0091ddbdf3d4b9d99c9942d6e8
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-21 23:15:42 +0300

    initial commit

---
+++ ninjalist/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+}
