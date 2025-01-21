93e41b4cb386bf3c4d893a9b2de5105d7b02de4d
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-22 00:47:45 +0300

    added lecture 5: Creating a Layout Component

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -7,12 +7,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar></Navbar>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ea deleniti dolore! Modi quo vero hic ut. Perspiciatis maiores error exercitationem? Odit, molestiae repellendus architecto debitis provident officiis sint ducimus?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio reiciendis, voluptate blanditiis asperiores incidunt optio vel voluptatum maiores. Placeat aut accusantium fugit in, modi temporibus dolor unde adipisci beatae quasi?</p>
       <Link href="/ninjas">See Ninja Listing</Link>
-      <Footer></Footer>
     </div>
   );
 }
