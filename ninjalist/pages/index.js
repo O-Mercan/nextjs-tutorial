3ddaa039cbcc7289b87a85120b33cde3e75332b8
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-22 00:20:49 +0300

    added lecture 4: Linking Between Pages

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ea deleniti dolore! Modi quo vero hic ut. Perspiciatis maiores error exercitationem? Odit, molestiae repellendus architecto debitis provident officiis sint ducimus?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio reiciendis, voluptate blanditiis asperiores incidunt optio vel voluptatum maiores. Placeat aut accusantium fugit in, modi temporibus dolor unde adipisci beatae quasi?</p>
+      <Link href="/ninjas">See Ninja Listing</Link>
       <Footer></Footer>
     </div>
   );
