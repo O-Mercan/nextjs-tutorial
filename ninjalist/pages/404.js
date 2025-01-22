598259aef447efcd17d4a2c0b3d32a4b34a1360a
Ömer Mercan <70708913+O-Mercan@users.noreply.github.com>
2025-01-22 13:35:42 +0300

    added lecture 9: Images and Metadata

--- ninjalist/pages/404.js
+++ ninjalist/pages/404.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
@@ -13,11 +14,17 @@ const NotFound = () => {
     }, [])
 
     return (
-        <div className="not-found">
-            <h1>Ooooopsss....</h1>
-            <h2>That page cannot found.</h2>
-            <p>Go back to the <Link href="/">Homepage</Link></p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | 404</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div className="not-found">
+                <h1>Ooooopsss....</h1>
+                <h2>That page cannot found.</h2>
+                <p>Go back to the <Link href="/">Homepage</Link></p>
+            </div>
+        </>
     );
 }
  
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -6,11 +6,17 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ea deleniti dolore! Modi quo vero hic ut. Perspiciatis maiores error exercitationem? Odit, molestiae repellendus architecto debitis provident officiis sint ducimus?</p>
-      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio reiciendis, voluptate blanditiis asperiores incidunt optio vel voluptatum maiores. Placeat aut accusantium fugit in, modi temporibus dolor unde adipisci beatae quasi?</p>
-      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>  
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ea deleniti dolore! Modi quo vero hic ut. Perspiciatis maiores error exercitationem? Odit, molestiae repellendus architecto debitis provident officiis sint ducimus?</p>
+        <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio reiciendis, voluptate blanditiis asperiores incidunt optio vel voluptatum maiores. Placeat aut accusantium fugit in, modi temporibus dolor unde adipisci beatae quasi?</p>
+        <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
+      </div>
+    </>
   );
 }
